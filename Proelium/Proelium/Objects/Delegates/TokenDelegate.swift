1c96fd42865a3cf5e73ff1c9452df82dd222c971
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2020-03-18 15:31:58 -0700

    Protocol-Token-Delegate -Added TokenDelegate

---
+++ Proelium/Proelium/Objects/Delegates/TokenDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TokenDelegate.swift
+//  Proelium
+//
+//  Created by Anthony Torres on 3/18/20.
+//  Copyright © 2020 John Torres. All rights reserved.
+//
+
+import Foundation
+
+protocol TokenDelegate {
+    func didTapToken(tokenNumber: Int)
+}
